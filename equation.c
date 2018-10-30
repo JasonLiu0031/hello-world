69872d44b39983e4346c75eaf7b83e6a2853da45
JasonLiu0031 <1198754299@qq.com>
2018-10-30 16:36:49 +0800

    homework

--- equation.c
+++ equation.c
@@ -2,3 +2,38 @@
 //ax + by = c
 //dx + ey = f
 //æ³¨æ„è¾“å…¥/è¾“å‡ºï¼Œæ•°æ®ç±»å‹
+
+#include<stdio.h>
+int main(void)
+{
+	float a,b,c,d,e,f,g,h,i;
+	float x,y;
+	printf("Çó½âax+by=c;dx+ey=fĞÍ¶şÔªÒ»´ÎÏßĞÔ·½³Ì×é\n");
+	printf("a=\n");
+	scanf("%f",&a);
+	printf("b=\n");
+	scanf("%f",&b);
+	printf("c=\n");
+	scanf("%f",&c);
+	printf("d=\n");
+	scanf("%f",&d);
+	printf("e=\n");
+	scanf("%f",&e);
+	printf("f=\n");
+	scanf("%f",&f);
+	g=a*e-b*d;     //|A|
+	   if (g==0)
+	printf("ÎŞ½â");
+	   else
+       { 
+	         h= c*e-b*f;     //|Dx| 
+ 	         i= a*f-c*d;     //|Dy|
+	         x= h/g;
+	         y= i/g;
+	         printf("x=%.3f\n",x);
+	         printf("y=%.3f\n",y);
+	   } 
+	return 0;
+	
+	
+}
--- equation1.c
+++ equation1.c
@@ -1,3 +1,34 @@
 //求解一元二次方程的根
 //查阅sqrt()函数
 //注意有没有解
+
+
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+	float a,b,c,d;
+	printf("���ax^2+bx+c=0��һԪ���η��̵Ľ�\n");
+	printf("a=\n");
+	scanf("%f",&a);
+	printf("b=\n");
+	scanf("%f",&b);
+	printf("c=\n");
+	scanf("%f",&c);
+	d=b*b-4*a*c;
+	   if(d<0)
+	     printf("�޽�");
+		   else
+		     {
+		     	float e,f,g;
+		     	e=-b+sqrt(d);
+		     	f=-b-sqrt(d);
+		     	g=2*a;
+		     	float x1,x2;
+		     	x1=e/g;
+		     	x2=f/g;
+		     	printf("x1=%.3f\n",x1);
+		     	printf("x2=%.3f\n",x2);
+			  } 
+	return 0;
+}
